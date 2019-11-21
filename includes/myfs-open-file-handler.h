47c803e10201f5bc0a07b9fac47a040ca7888271
Alexander Brese <admin@alexanderbrese.com>
2019-11-21 19:46:59 +0100

    Implement missing fuse operations.

--- includes/myfs-open-file-handler.h
+++ includes/myfs-open-file-handler.h
@@ -10,13 +10,29 @@
 class MyFsOpenFileHandler {
 private:
     MyFsOpenFileHandle *openFileHandles;
+    static const int NOT_OPEN = -1;
 
 public:
     MyFsOpenFileHandler();
     ~MyFsOpenFileHandler();
 
     void init(MyFsOpenFileHandle * openFileHandles);
-    void openFile(int fileDescriptor, MyFsFileAccessMode accessMode, struct fuse_file_info *fileInfo);
+
+    bool hasFreeSpace();
+
+    int openFile(int fileDescriptor, MyFsFileAccessMode accessMode);
+
+    bool isValidOpenFileHandle(int openFileHandleDescriptor);
+
+    bool isOpen(int openFileHandleDescriptor);
+
+    bool isRead(int openFileHandleDescriptor);
+
+    bool isWrite(int openFileHandleDescriptor);
+
+    int getFileDescriptor(int openFileHandleDescriptor);
+
+    void release(int openFileHandleDescriptor);
 };
 
 #endif //MYFS_MYFS_OPEN_FILE_HANDLER_H
--- src/myfs-open-file-handler.cpp
+++ src/myfs-open-file-handler.cpp
@@ -2,6 +2,7 @@
 // Created by ahlex on 20.11.2019.
 //
 
+#include <fuse_common.h>
 #include "myfs-open-file-handler.h"
 
 MyFsOpenFileHandler::MyFsOpenFileHandler() = default;
@@ -15,11 +16,55 @@ void MyFsOpenFileHandler::init(MyFsOpenFileHandle * openFileHandles) {
 
     // INITIALIZE ALL OPEN FILE HANDLES
     for (int i = 0; i < NUM_OPEN_FILES; i++) {
-        openFileHandles[i].fileDescriptor = -1;
+        release(i);
     }
 }
 
-void MyFsOpenFileHandler::openFile(int fileDescriptor, MyFsFileAccessMode accessMode, struct fuse_file_info *fileInfo) {
+bool MyFsOpenFileHandler::hasFreeSpace() {
+    for (int i = 0; i < NUM_OPEN_FILES; i++) {
+        if (openFileHandles[i].fileDescriptor < 0) {
+            return true;
+        }
+    }
 
+    return false;
 }
 
+int MyFsOpenFileHandler::openFile(int fileDescriptor, MyFsFileAccessMode accessMode) {
+    for (int i = 0; i < NUM_OPEN_FILES; i++) {
+        openFileHandles[i].fileDescriptor = fileDescriptor;
+        openFileHandles[i].accessMode = accessMode;
+        return i;
+    }
+}
+
+bool MyFsOpenFileHandler::isValidOpenFileHandle(int openFileHandleDescriptor) {
+    return openFileHandleDescriptor >= 0 && openFileHandleDescriptor < NUM_OPEN_FILES;
+}
+
+bool MyFsOpenFileHandler::isOpen(int openFileHandleDescriptor) {
+    MyFsOpenFileHandle openFileHandle = openFileHandles[openFileHandleDescriptor];
+    return openFileHandle.fileDescriptor != NOT_OPEN;
+}
+
+bool MyFsOpenFileHandler::isRead(int openFileHandleDescriptor) {
+    MyFsOpenFileHandle openFileHandle = openFileHandles[openFileHandleDescriptor];
+    return openFileHandle.accessMode.read;
+}
+
+bool MyFsOpenFileHandler::isWrite(int openFileHandleDescriptor) {
+    MyFsOpenFileHandle openFileHandle = openFileHandles[openFileHandleDescriptor];
+    return openFileHandle.accessMode.write;
+}
+
+int MyFsOpenFileHandler::getFileDescriptor(int openFileHandleDescriptor) {
+    return openFileHandles[openFileHandleDescriptor].fileDescriptor;
+}
+
+void MyFsOpenFileHandler::release(int openFileHandleDescriptor) {
+    openFileHandles[openFileHandleDescriptor].fileDescriptor = NOT_OPEN;
+    MyFsFileAccessMode accessMode = {};
+    accessMode.read = false;
+    accessMode.write = false;
+    openFileHandles[openFileHandleDescriptor].accessMode = accessMode;
+}
